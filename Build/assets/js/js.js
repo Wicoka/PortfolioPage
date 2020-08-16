9d7252c8a5a3a2304ed20af3b451d15220941e59
Szőke Viktor <szoke.viktor@simonyi.bme.hu>
2020-08-16 15:10:14 +0200

    fix: Dark mode fix

--- Build/assets/js/js.js
+++ Build/assets/js/js.js
@@ -1,15 +1,24 @@
-$(document).ready(function() {
-	$(".navbar .navbar-nav .nav-item a").click(function() {
+$(document).ready(function () {
+	$(".navbar .navbar-nav .nav-item a").click(function () {
 		$("html, body").animate({
 			scrollTop: $($.attr(this, "href")).offset().top
 		}, 1);
 	});
+	var moonElement = document.getElementById("moon");
+	var sunElement = document.getElementById("sun");
+	if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		sunElement.style.display = "flex";
+		moonElement.style.display = "none";
+	} else {
+		sunElement.style.display = "none";
+		moonElement.style.display = "flex";
+	}
+
 });
 
 
 var isDarkMode;
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    // dark mode
 	this.isDarkMode = true;
 } else {
 	this.isDarkMode = false;
@@ -17,6 +26,15 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 
 function toggleDarkMode() {
 	isDarkMode = !isDarkMode;
+	var moonElement = document.getElementById("moon");
+	var sunElement = document.getElementById("sun");
+	if (isDarkMode) {
+		sunElement.style.display = "flex";
+		moonElement.style.display = "none";
+	} else {
+		sunElement.style.display = "none";
+		moonElement.style.display = "flex";
+	}
 	var element = document.body;
 	element.classList.toggle("dark-mode");
 }
