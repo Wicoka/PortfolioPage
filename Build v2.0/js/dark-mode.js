79ebee2cde8bb5cdb7977ab8bb1fab36306f7df5
Szőke Viktor <viktor.szoke@schdesign.hu>
2020-12-23 22:52:26 +0100

    Dark mode toggle

--- Build v2.0/js/dark-mode.js
+++ Build v2.0/js/dark-mode.js
@@ -1,15 +1,30 @@
 const theme = document.querySelector("#theme-link");
 var isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
+function initDarkMode() {
+    if (isDarkMode) {
+        // Switch to light mode
+        document.getElementById("dark-mode-checkbox").checked = isDarkMode;
+        theme.href = "css/dark-style.css";
+    } else {
+        // Switch to dark mode
+        document.getElementById("dark-mode-checkbox").checked = isDarkMode;
+        theme.href = "";
+    }
+}
+
 function toggleDarkMode() {
-    console.log('is it dark mode?', isDarkMode);
     if (isDarkMode) {
         // Switch to light mode
         isDarkMode = !isDarkMode;
+        document.getElementById("dark-mode-checkbox").checked = isDarkMode;
         theme.href = "";
     } else {
         // Switch to dark mode
         isDarkMode = !isDarkMode;
+        document.getElementById("dark-mode-checkbox").checked = isDarkMode;
         theme.href = "css/dark-style.css";
     }
 }
+
+initDarkMode();
