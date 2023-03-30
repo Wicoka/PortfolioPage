4dfb66cb0a6093418aa15ca15ee9289c99b22602
Viktor Szőke <viktor.szoke@schdesign.hu>
2023-03-31 00:05:56 +0200

    Add typed.js

--- angular/portfolio/src/app/pages/main-page/main-page.component.ts
+++ angular/portfolio/src/app/pages/main-page/main-page.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   // TODO - Add Typed.js
   // https://www.jsdelivr.com/package/npm/typed.js
   // https://boxicons.com/usage
 
   date = new Date();
+
+  constructor() {}
+  ngOnInit(): void {
+    const typed = new Typed('#element', {
+      strings: ['Frontend Developer', 'Team Leader', 'Casual Gamer'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 1000,
+      loop: true,
+    });
+  }
 }
