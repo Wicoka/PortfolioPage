375279dc25dc1605a20c9414e9b1832252b73dd7
Viktor Szőke <viktor.szoke@schdesign.hu>
2023-03-30 15:55:14 +0200

    Add Vercel Analytics

--- angular/portfolio/src/app/app.component.ts
+++ angular/portfolio/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor() {
+    inject();
+  }
 }
